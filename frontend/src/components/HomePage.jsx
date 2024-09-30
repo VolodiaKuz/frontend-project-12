0fc0f387a8bc927ff721ea3524b2198f1d18d878
Volodia K <wtraktorman94@mail.ru>
2024-10-01 00:16:58 +0500

    add routes

---
+++ frontend/src/components/HomePage.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+function HomePage() {
+  return (
+    <>
+      <div class='h-100 bg-light'>
+        <h1>Test route '/'</h1>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
