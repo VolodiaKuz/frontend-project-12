c1a33f92d3c7c51af72389aff4606dc3b085bb9d
Volodia K <wtraktorman94@mail.ru>
2024-10-18 00:18:17 +0500

    update NotFoundPage

--- frontend/src/Pages/NotFoundPage.jsx
+++ frontend/src/Pages/NotFoundPage.jsx
@@ -1,3 +1,5 @@
+import Navbar from '../components/Navbar';
+
 function NotFoundPage() {
   return (
     <>
@@ -5,16 +7,7 @@ function NotFoundPage() {
         <div className='h-100'>
           <div className='h-100' id='chat'>
             <div className='d-flex flex-column h-100'>
-              <nav className='shadow-sm navbar navbar-expand-lg navbar-light bg-white'>
-                <div className='container'>
-                  <a className='navbar-brand' href='/'>
-                    Hexlet Chat
-                  </a>
-                  <button type='button' className='btn btn-primary'>
-                    Выйти
-                  </button>
-                </div>
-              </nav>
+              <Navbar homePage={false} />
               <div className='text-center'>
                 <img
                   alt='Страница не найдена'
