f34e5ba96457080b372680f0e4f194adcf43d040
Volodia K <wtraktorman94@mail.ru>
2024-10-28 23:54:06 +0500

    add AuthContext

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,18 +1,43 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import LoginPage from '../Pages/LoginPage';
 import SignUpPage from '../Pages/SignUpPage.jsx';
 import HomePage from '../Pages/HomePage';
 import NotFoundPage from '../Pages/NotFoundPage';
+import AuthContext from '../contexts/index.jsx';
+// import useAuth from '../hooks/index.jsx';
+
+const AuthProvider = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const logIn = () => {
+    setLoggedIn(true);
+    console.log('test AuthProvider');
+  };
+  const logOut = () => {
+    localStorage.removeItem('userId');
+    setLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ loggedIn, logIn, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
-  </BrowserRouter>
+  <AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  </AuthProvider>
 );
 
 export default App;
