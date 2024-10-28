696adeb800683f7d515ff7fa98df7609d38c2819
Volodia K <wtraktorman94@mail.ru>
2024-10-28 19:40:07 +0500

    separate Channel component from Channels

---
+++ frontend/src/components/Channel.jsx
@@ -0,0 +1,82 @@
+import { Button, Dropdown } from 'react-bootstrap';
+import filter from 'leo-profanity';
+import { useSelector } from 'react-redux';
+import { countMessages } from '../store/messagesSlice.js';
+import store from '../store/index.js';
+import { setActive } from '../store/channelsSlice.js';
+
+const handleActiveChannel = (channel) => {
+  const { dispatch } = store;
+  dispatch(setActive({ channel }));
+  const { messages } = store.getState().messagesStore;
+  const test = messages.filter((el) => el.channelId === channel.id);
+  dispatch(countMessages({ count: test.length }));
+};
+
+const renderRemovableChannel = (
+  channel,
+  setModalInfo,
+  activeChannel,
+) => {
+  const buttonClasses = 'w-100 rounded-0 text-start';
+  const buttonVariant = `${channel.id === activeChannel.id ? 'secondary' : 'light'}`;
+
+  return (
+    <li key={channel.id}>
+      <Dropdown className={buttonClasses}>
+        <Button
+          variant={buttonVariant}
+          onClick={() => handleActiveChannel(channel)}
+        >
+          #
+          {' '}
+          {filter.clean(channel.name)}
+        </Button>
+
+        <Dropdown.Toggle split variant={buttonVariant} id="dropdown-split-basic">
+          <span className="visually-hidden">Управление каналом</span>
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item onClick={() => setModalInfo(({ type: 'remove', item: channel }))}>Удалить</Dropdown.Item>
+          <Dropdown.Item onClick={() => setModalInfo(({ type: 'rename', item: channel }))}>Переименовать</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </li>
+  );
+};
+
+const Channel = ({ setModalInfo }) => {
+  // const dispatch = useDispatch();
+  const channels = useSelector((state) => state.channelsStore.channels);
+  const activeChannel = useSelector((state) => state.channelsStore.activeChannel);
+
+  const channelsHtml = channels.map((channel) => {
+    const buttonClasses = `w-100 rounded-0 text-start btn ${channel.id === activeChannel.id ? 'btn-secondary' : ''}`;
+    const buttonVariant = `${channel.id === activeChannel.id ? 'secondary' : 'light'}`;
+    if (channel.removable) {
+      return renderRemovableChannel(
+        channel,
+        setModalInfo,
+        activeChannel,
+      );
+    }
+    const channelHtml = (
+      <li key={channel.id}>
+        <Button
+          variant={buttonVariant}
+          className={buttonClasses}
+          onClick={() => handleActiveChannel(channel)}
+        >
+          #
+          {' '}
+          {channel.name}
+        </Button>
+      </li>
+    );
+    return channelHtml;
+  });
+  return channelsHtml;
+};
+
+export default Channel;
--- frontend/src/components/Channels.jsx
+++ frontend/src/components/Channels.jsx
@@ -1,90 +1,10 @@
 import { PlusSquare } from 'react-bootstrap-icons';
-import { Button, Dropdown } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
-// import { toast } from 'react-toastify';
-import filter from 'leo-profanity';
 import axios from 'axios';
-import { useDispatch, useSelector } from 'react-redux';
-// import { current } from '@reduxjs/toolkit';
-import { fillChannels, setActive } from '../store/channelsSlice.js';
-import { countMessages } from '../store/messagesSlice.js';
-import store from '../store/index.js';
+import { useDispatch } from 'react-redux';
+import { fillChannels } from '../store/channelsSlice.js';
 import Modal from './Modal.jsx';
-
-const handleActiveChannel = (channel) => {
-  const { dispatch } = store;
-  dispatch(setActive({ channel }));
-  const { messages } = store.getState().messagesStore;
-  const test = messages.filter((el) => el.channelId === channel.id);
-  dispatch(countMessages({ count: test.length }));
-};
-
-const renderRemovableChannel = (
-  channel,
-  setModalInfo,
-  activeChannel,
-) => {
-  const buttonClasses = 'w-100 rounded-0 text-start';
-  const buttonVariant = `${channel.id === activeChannel.id ? 'secondary' : 'light'}`;
-
-  return (
-    <li key={channel.id}>
-      <Dropdown className={buttonClasses}>
-        <Button
-          variant={buttonVariant}
-          onClick={() => handleActiveChannel(channel)}
-        >
-          #
-          {' '}
-          {filter.clean(channel.name)}
-        </Button>
-
-        <Dropdown.Toggle split variant={buttonVariant} id="dropdown-split-basic">
-          <span className="visually-hidden">Управление каналом</span>
-        </Dropdown.Toggle>
-
-        <Dropdown.Menu>
-          <Dropdown.Item onClick={() => setModalInfo(({ type: 'remove', item: channel }))}>Удалить</Dropdown.Item>
-          <Dropdown.Item onClick={() => setModalInfo(({ type: 'rename', item: channel }))}>Переименовать</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-    </li>
-  );
-};
-
-const RenderChannels = ({ setModalInfo }) => {
-  // const dispatch = useDispatch();
-  const channels = useSelector((state) => state.channelsStore.channels);
-  const activeChannel = useSelector((state) => state.channelsStore.activeChannel);
-  console.log('activeChannel=>', activeChannel);
-
-  const channelsHtml = channels.map((channel) => {
-    const buttonClasses = `w-100 rounded-0 text-start btn ${channel.id === activeChannel.id ? 'btn-secondary' : ''}`;
-    const buttonVariant = `${channel.id === activeChannel.id ? 'secondary' : 'light'}`;
-    if (channel.removable) {
-      return renderRemovableChannel(
-        channel,
-        setModalInfo,
-        activeChannel,
-      );
-    }
-    const channelHtml = (
-      <li key={channel.id}>
-        <Button
-          variant={buttonVariant}
-          className={buttonClasses}
-          onClick={() => handleActiveChannel(channel)}
-        >
-          #
-          {' '}
-          {channel.name}
-        </Button>
-      </li>
-    );
-    return channelHtml;
-  });
-  return channelsHtml;
-};
+import Channel from './Channel.jsx';
 
 const Channels = () => {
   const [modalInfo, setModalInfo] = useState({ type: null, item: null });
@@ -93,7 +13,6 @@ const Channels = () => {
 
   useEffect(() => {
     const uploadChannels = async () => {
-      console.log(localStorage.getItem('userId'));
       if (!localStorage.getItem('userId')) return [];
       const { token } = JSON.parse(localStorage.getItem('userId'));
 
@@ -102,7 +21,6 @@ const Channels = () => {
           Authorization: `Bearer ${token}`,
         },
       });
-      console.log('result.data ====> ', result.data);
       const createdChannels = result.data;
       dispatch(fillChannels({ createdChannels }));
       return null;
@@ -128,7 +46,7 @@ const Channels = () => {
         id="channels-box"
         className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block"
       >
-        <RenderChannels setModalInfo={setModalInfo} />
+        <Channel setModalInfo={setModalInfo} />
       </ul>
       <Modal modalInfo={modalInfo} hideModal={hideModal} />
     </div>
