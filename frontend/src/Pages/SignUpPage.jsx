8b0d9410e862da11a6ac798d483c5957ffa9e8f7
Volodia K <wtraktorman94@mail.ru>
2024-10-30 01:44:02 +0500

    add FormContainer to SignUpPage

--- frontend/src/Pages/SignUpPage.jsx
+++ frontend/src/Pages/SignUpPage.jsx
@@ -1,40 +1,10 @@
-import { useNavigate } from 'react-router-dom';
-import Navbar from '../components/Navbar';
 import SignUpForm from '../components/Forms/SignUpForm';
+import FormContainer from '../components/Forms/FormContainer';
 
-const LoginPage = () => {
-  const navigate = useNavigate();
-
-  return (
-    <div className="h-100 bg-light">
-      <div className="h-100">
-        <div className="h-100" id="chat">
-          <div className="d-flex flex-column h-100">
-            <Navbar navigate={navigate} homePage={false} />
-            <div className="container-fluid h-100">
-              <div className="row justify-content-center align-content-center h-100">
-                <div className="col-12 col-md-8 col-xxl-6">
-                  <div className="card shadow-sm">
-                    <div className="card-body row p-5">
-                      <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
-                        <img
-                          src="auth_logo.jpeg"
-                          className="rounded-circle"
-                          alt="Войти"
-                        />
-                      </div>
-                      <SignUpForm />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="Toastify" />
-        </div>
-      </div>
-    </div>
-  );
-};
+const LoginPage = () => (
+  <FormContainer>
+    <SignUpForm />
+  </FormContainer>
+);
 
 export default LoginPage;
