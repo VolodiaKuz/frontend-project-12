adde542f84ea47d7b13ac5d4be62f1384b865354
Volodia K <wtraktorman94@mail.ru>
2024-10-31 15:08:20 +0500

    add translation locale to notFoundPage

--- frontend/src/Pages/NotFoundPage.jsx
+++ frontend/src/Pages/NotFoundPage.jsx
@@ -1,15 +1,20 @@
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import routes from '../utils/routes';
 import PageContainer from '../components/PageContainer';
 
-const NotFoundPage = () => (
-  <PageContainer type="notFound" notFound>
-    <h1 className="h4 text-muted d-flex align-items-center justify-content-center">Страница не найдена</h1>
-    <p className="text-muted d-flex align-items-center justify-content-center">
-      Но вы можете перейти
-    </p>
-    <Link to={routes.mainPagePath()} className="d-flex align-items-center justify-content-center">на главную страницу </Link>
-  </PageContainer>
-);
+const NotFoundPage = () => {
+  const { t } = useTranslation();
+
+  return (
+    <PageContainer type="notFound" notFound>
+      <h1 className="h4 text-muted d-flex align-items-center justify-content-center">{t('notFound.header')}</h1>
+      <p className="text-muted d-flex align-items-center justify-content-center">
+        {t('notFound.message')}
+      </p>
+      <Link to={routes.mainPagePath()} className="d-flex align-items-center justify-content-center">{t('notFound.linkText')}</Link>
+    </PageContainer>
+  );
+};
 
 export default NotFoundPage;
