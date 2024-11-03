73838bf8e41a1b08932ed64e4103f6e4b93c9e13
Volodia K <wtraktorman94@mail.ru>
2024-11-03 17:54:05 +0500

    add groupping of the imports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import init from './utils/init.js';
 import 'react-toastify/dist/ReactToastify.css';
+
+import init from './utils/init.js';
 import store from './store/index.js';
 
 const initApp = async () => {
