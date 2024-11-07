55c609ad4ff62a3031dd16032622a926afef355d
Volodia K <wtraktorman94@mail.ru>
2024-11-07 21:10:09 +0500

    add token check in PrivateRoute after refresh

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable consistent-return */
 import {
   BrowserRouter,
   Routes,
   Route,
   Navigate,
 } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
 import useAuth from '../auth/authHook.jsx';
 import LoginPage from '../Pages/LoginPage';
@@ -12,14 +15,28 @@ import HomePage from '../Pages/HomePage';
 import NotFoundPage from '../Pages/NotFoundPage';
 import routes from '../utils/routes';
 import AuthProvider from '../auth/AuthProvider.jsx';
+import { addToken } from '../store/userSlice.js';
 
 const PrivateRoute = ({ children }) => {
   const auth = useAuth();
-  console.log('auth.loggedIn ?=>', auth.loggedIn);
+  const dispatch = useDispatch();
 
-  return (
-    auth.loggedIn ? children : <Navigate to="/login" />
-  );
+  useEffect(() => {
+    if (!auth.loggedIn) {
+      const userId = JSON.parse(localStorage.getItem('userId'));
+      if (!userId) {
+        return <Navigate to="/login" />;
+      }
+      auth.logIn();
+      const user = userId;
+      dispatch(addToken({ user }));
+    }
+  });
+
+  return children;
+  // return (
+  //   auth.loggedIn ? children : <Navigate to="/login" />
+  // );
 };
 
 const App = () => (
