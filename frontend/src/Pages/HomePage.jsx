2efc9260fcd50490d351214235c029edf748dd06
Volodia K <wtraktorman94@mail.ru>
2024-11-10 20:31:33 +0500

    remove Chat component

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,5 +1,33 @@
-import Chat from '../components/Chat';
+import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
 
-const HomePage = () => <Chat />;
+import Navbar from '../components/Navbar';
+import Channels from '../components/Channels';
+import Messages from '../components/Messages';
+
+const HomePage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="h-100 bg-light">
+      <ToastContainer />
+      <div className="h-100">
+        <div className="h-100" id="chat">
+          <div className="d-flex flex-column h-100">
+            <Navbar navigate={navigate} homePage />
+            <div className="container h-100 my-4 overflow-hidden rounded shadow">
+              <div className="row h-100 bg-white flex-md-row">
+                <Channels />
+                <Messages />
+              </div>
+            </div>
+          </div>
+          <div className="Toastify" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
