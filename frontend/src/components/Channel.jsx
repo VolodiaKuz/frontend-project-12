4cfe656285b24e136718369ac8124b852007b0ad
Volodia K <wtraktorman94@mail.ru>
2024-12-17 20:44:14 -0500

    fix edits 12-13

--- frontend/src/components/Channel.jsx
+++ frontend/src/components/Channel.jsx
@@ -1,71 +1,58 @@
 import { Button, Dropdown } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import filter from 'leo-profanity';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
-import store from '../store/index.js';
 import { countMessages } from '../store/messagesSlice.js';
 import { setActiveChannel } from '../store/userSlice.js';
 
-const handleActiveChannel = (channel) => {
-  const { dispatch } = store;
-  const { messages } = store.getState().messagesStore;
-  const channelMessages = messages.filter((el) => el.channelId === channel.id);
-  dispatch(countMessages({ count: channelMessages.length }));
-  dispatch(setActiveChannel({ channel }));
-};
-
-const renderRemovableChannel = (
-  channel,
-  setModalInfo,
-  activeChannel,
-  t,
-) => {
-  const buttonClasses = 'd-flex rounded-0 text-start';
-  const buttonVariant = `${channel.id === activeChannel.id ? 'secondary' : 'light'}`;
-
-  return (
-    <li key={channel.id}>
-      <Dropdown className={buttonClasses}>
-        <Button
-          className="w-100 rounded-0 text-start text-truncate"
-          variant={buttonVariant}
-          onClick={() => handleActiveChannel(channel)}
-        >
-          #
-          {' '}
-          {filter.clean(channel.name)}
-        </Button>
-
-        <Dropdown.Toggle split variant={buttonVariant} id="dropdown-split-basic">
-          <span className="visually-hidden">{t('channels.menu')}</span>
-        </Dropdown.Toggle>
-
-        <Dropdown.Menu>
-          <Dropdown.Item onClick={() => setModalInfo(({ type: 'remove', item: channel }))}>{t('channels.remove')}</Dropdown.Item>
-          <Dropdown.Item onClick={() => setModalInfo(({ type: 'rename', item: channel }))}>{t('channels.rename')}</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-    </li>
-  );
-};
-
 const Channel = ({ setModalInfo }) => {
   const channels = useSelector((state) => state.channelsStore.channels);
+  const messages = useSelector((state) => state.messagesStore.messages);
   const activeChannel = useSelector((state) => state.userStore.activeChannel);
   const { t } = useTranslation();
+  const dispatch = useDispatch();
+
+  const handleActiveChannel = (channel) => {
+    // const { dispatch } = store;
+    // const { messages } = store.getState().messagesStore;
+    const channelMessages = messages.filter((el) => el.channelId === channel.id);
+    dispatch(countMessages({ count: channelMessages.length }));
+    dispatch(setActiveChannel({ channel }));
+  };
 
   const channelsHtml = channels.map((channel) => {
-    const buttonClasses = `w-100 rounded-0 text-start btn ${channel.id === activeChannel.id ? 'btn-secondary' : ''}`;
-    const buttonVariant = `${channel.id === activeChannel.id ? 'secondary' : 'light'}`;
     if (channel.removable) {
-      return renderRemovableChannel(
-        channel,
-        setModalInfo,
-        activeChannel,
-        t,
+      const removableChannelbuttonClasses = 'd-flex rounded-0 text-start';
+      const removableChannelbuttonVariant = `${channel.id === activeChannel.id ? 'secondary' : 'light'}`;
+
+      return (
+        <li key={channel.id}>
+          <Dropdown className={removableChannelbuttonClasses}>
+            <Button
+              className="w-100 rounded-0 text-start text-truncate"
+              variant={removableChannelbuttonVariant}
+              onClick={() => handleActiveChannel(channel)}
+            >
+              #
+              {' '}
+              {filter.clean(channel.name)}
+            </Button>
+
+            <Dropdown.Toggle split variant={removableChannelbuttonVariant} id="dropdown-split-basic">
+              <span className="visually-hidden">{t('channels.menu')}</span>
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={() => setModalInfo(({ type: 'remove', item: channel }))}>{t('channels.remove')}</Dropdown.Item>
+              <Dropdown.Item onClick={() => setModalInfo(({ type: 'rename', item: channel }))}>{t('channels.rename')}</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </li>
       );
     }
+    const buttonClasses = `w-100 rounded-0 text-start btn ${channel.id === activeChannel.id ? 'btn-secondary' : ''}`;
+    const buttonVariant = `${channel.id === activeChannel.id ? 'secondary' : 'light'}`;
     const channelHtml = (
       <li key={channel.id}>
         <Button
